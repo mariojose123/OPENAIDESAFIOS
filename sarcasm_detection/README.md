7e28ca0ca7637ae986d141ada9f359dfc3a81c1f
mariojose123 <mariofilho4k@gmail.com>
2016-11-20 23:08:49 -0200

    finished just turn all strings in the list into a beatiful csv

---
+++ sarcasm_detection/README.md
@@ -0,0 +1,3 @@
+# Sarcasm Detection
+
+Sarcasm is a form of verbal irony that is intended to express contempt or ridicule. Our goal in this project is explore deeep learning models, such as Word2vect and Long short-term memory (LSTM), to automact detection of sarcasm. 
---
+++ sarcasm_detection/web_crawler.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+This is a crawler to get quotes by tags
+Thaaks goodreads.com ;) 
+"""
+
+
+
+
+from bs4 import BeautifulSoup
+import urllib2
+import pandas as pd
+
+
+
+#how to use beautifulsoup
+#http://www.pythonforbeginners.com/python-on-the-web/web-scraping-with-beautifulsoup
+
+def StripTags(text):
+        finished = 0
+        text=text.split("//<![CDATA[")
+        text=str(text[0].encode('utf-8'))
+        text=text.split('―')
+        text=str(text[0])
+        return text
+                    
+
+def get_quotes(max_pages, tag, ):
+	quotes=[]
+	for i in xrange(max_pages):
+		url="https://www.goodreads.com/quotes/tag/"+str(tag)+"?page="+str(i)+"&utf8=%E2%9C%93"
+		print url
+		page=urllib2.urlopen(url)
+		soup = BeautifulSoup(page.read(),'html.parser')
+		mydivs = soup.findAll("div", { "class" : "quoteText" })
+		for div in mydivs:
+			quotes.append(StripTags(div.text))
+		
+
+	return quotes
+
+
+##################
+if __name__ == "__main__":
+	max_pages=2
+	tag="sarcastic"
+	dataset_output="sarcasm.csv"
+
+	#crawling
+	quotes = get_quotes(max_pages, tag)
+
+	#saving to csv
+	for q in quotes:
+		print q
